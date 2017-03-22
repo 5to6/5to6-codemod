79890722d60e4e9862f5a958f4fa01d14964bd39
Jason Moon <jsnmoon@me.com>
2017-03-22 12:17:16 -0700

    Add named export generation
    For a default object export, this transformation will append named exports corresponding to the object property keys.
    
    Squashed changes:
    - Ensure VariableDeclarator has an ObjectExpression as its init.
    - Ensure ObjectExpression’s property is lengthy before processing.

---
+++ transforms/named-export-generation.js
@@ -0,0 +1,179 @@
+/**
+ * named-export-generation - Given a default export object, generate named exports
+ * corresponding to object properties.
+ */
+
+var util = require('../utils/main');
+
+module.exports = function(file, api) {
+	var j = api.jscodeshift;
+	var root = j(file.source);
+	var firstNode = root.find(j.Program).get('body', 0).node
+
+	/**
+	 * Add named exports for default exported declaration
+	 */
+	function addNamedExports(exportRef) {
+		var defaultExportName;
+		var objectExpressions = [];
+
+		// Find default exported object expression
+		// ex. export default { ... };
+		if (exportRef.value.declaration.type === 'ObjectExpression') {
+			// TODO: Write function to generate exported declarator name
+			defaultExportName = generateDefaultExportName()
+			var replacementExport = j.exportDefaultDeclaration(j.identifier(defaultExportName));
+			var replacementDeclaration = j.variableDeclaration('const', [
+				j.variableDeclarator(j.identifier(defaultExportName), exportRef.value.declaration)
+			]);
+			if (exportRef.value.comments && exportRef.value.comments.length > 0) {
+				replacementDeclaration.comments = exportRef.value.comments;
+			}
+			exportRef.insertBefore(replacementDeclaration);
+			exportRef = j(exportRef).replaceWith(replacementExport).__paths[0];
+		} else {
+			defaultExportName = exportRef.value.declaration.name
+		}
+
+
+		// Find default exported declaration
+		// ex. const analytics = { ... };
+		//     export default analytics;
+		root.find(j.VariableDeclarator, {
+			id: {
+				type: 'Identifier',
+				name: defaultExportName
+			},
+			init: {
+				type: 'ObjectExpression'
+			}
+		}).forEach(function(vdRef) {
+			var objectExpression = vdRef.value.init;
+			if (objectExpression !== null) {
+				objectExpressions.push(objectExpression);
+			}
+		});
+
+		// Find default exported VariableDeclarator modified by ExpressionStatement
+		// ex. let analytics = { ... };      // Initialized
+		//     analytics = { ... };          // Updated to different value
+		//     analytics.someKey = 'horse';  // Updated to different value
+		//     export default analytics;
+		var mutatedProps = {};
+		root.find(j.ExpressionStatement, {
+			expression: {
+				operator: '=',
+				left: {
+					name: defaultExportName,
+					type: 'Identifier'
+				},
+				right: {
+					type: 'ObjectExpression'
+				}
+			}
+		}).forEach(function(esRef) {
+			var objectExpression = esRef.value.expression.right
+			objectExpression.properties.forEach(function(prop) {
+				mutatedProps[prop.key.name] = prop
+			});
+		});
+		root.find(j.ExpressionStatement, {
+			expression: {
+				operator: '=',
+				left: {
+					object: {
+						name: defaultExportName
+					},
+					type: 'MemberExpression'
+				}
+			}
+		}).forEach(function(esRef) {
+			var name = esRef.value.expression.left.property.name
+			var prop = j.property('init', j.identifier(name), j.identifier(name));
+			prop.shorthand = true;
+			mutatedProps[name] = prop;
+		});
+		if (Object.keys(mutatedProps).length > 0) {
+			var newObjectExpression = j.objectExpression(
+				Object.keys(mutatedProps).map(function(key) {
+					return mutatedProps[key]
+				})
+			)
+			objectExpressions.push(newObjectExpression)
+		}
+
+		// Convert object expressions into named export declarations and insert into tree
+		objectExpressions.filter(function(objectExpression) {
+			return 'properties' in objectExpression && objectExpression.properties.length > 0;
+		}).map(function(objectExpression) {
+			return generateNamedExportDeclaration(exportRef, defaultExportName, objectExpression);
+		}).forEach(function(exportNamedDeclaration) {
+			exportRef.insertAfter(exportNamedDeclaration);
+		});
+	}
+
+  function generateDefaultExportName() {
+    var prefix = 'exported';
+    var index = 2;
+    var name = prefix;
+    while (root.find(j.Identifier, { name: name }).length > 0) {
+      name = prefix + index;
+      index++;
+    }
+    return name;
+  }
+
+  function generateNamedExportDeclaration(exportRef, defaultExportName, objectExpression) {
+    const exportNames = objectExpression.properties.map(function(prop) {
+      return prop.key.name
+    });
+    const properties = exportNames.filter(function(name) {
+      return shouldCreateNamedExport(defaultExportName, name)
+    }).map(function(name) {
+      const prop = j.property('init', j.identifier(name), j.identifier(name));
+      prop.shorthand = true;
+      return prop;
+    });
+    const exportNamedDeclaration = j.exportNamedDeclaration(
+      j.variableDeclaration('const',[
+        j.variableDeclarator(
+          j.objectPattern(properties),
+          exportRef.value.declaration
+        )
+      ])
+    );
+    return exportNamedDeclaration;
+  }
+
+  function shouldCreateNamedExport(defaultExportName, exportName) {
+    const isAlreadyExportedInDeclaration = root.find(j.Property, {
+      value: {
+        name: exportName
+      }
+    }).filter(function(r) {
+      return util.hasParentOfType(r, 'ExportNamedDeclaration');
+    }).length > 0;
+    const isAlreadyExported = root.find(j.VariableDeclarator, {
+      id: { name: exportName },
+      init: {
+        object: { name: defaultExportName },
+        property: { name: exportName }
+      }
+    }).filter(function (r) {
+      return util.hasParentOfType(r, 'ExportNamedDeclaration');
+    }).length > 0;
+    const shouldCreateExport = !(isAlreadyExported || isAlreadyExportedInDeclaration);
+    return shouldCreateExport;
+  }
+
+	root.find(j.ExportDefaultDeclaration).forEach(addNamedExports);
+
+	// re-add comment to to the top if necessary
+	var firstNode2 = root.find(j.Program).get('body', 0).node
+	if (firstNode !== firstNode2) {
+		firstNode2.comments = firstNode.leadingComments
+	}
+
+	// FIXME: Use config instead of hardcoded value
+	return root.toSource({ quote: 'single' });
+};
