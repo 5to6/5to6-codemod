e53c406d989465c03abc3670020dcf9f82783b1c
Jason Moon <jsnmoon@me.com>
2017-05-22 13:04:08 -0600

    Tweak existing CJS tests
    Improve comments test and order of generated imports for called imports.

--- test/fixtures/cjs/called.after.js
+++ test/fixtures/cjs/called.after.js
@@ -4,15 +4,15 @@ import aFactory from 'var-stacked-comment';
 const a = aFactory();
 import bFactory from 'var-no-comment';
 const b = bFactory('arg for b');
-import cFactory from 'var-inline-comment'; // with inline comment
-const c = cFactory({ some: 'arg' });
+import cFactory from 'var-inline-comment';
+const c = cFactory({ some: 'arg' }); // with inline comment
 import deFactory from 'var-decomposition';
 const { d, e } = deFactory();
-import jFactory from 'multi-var-j';
-const j = jFactory();
-import hFactory from 'multi-var-h';
-const h = hFactory();
 import gFactory from 'multi-var-g';
 const g = gFactory();
+import hFactory from 'multi-var-h';
+const h = hFactory();
+import jFactory from 'multi-var-j';
+const j = jFactory();
 
 var f = 'x', unassigned, i = 'bar';
